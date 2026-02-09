dc00e6275566707cdf45147755e1b67192201798
gitorata <thegapjapan@gmail.com>
2026-02-09 17:42:27 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "FCアニース京都オフィシャルサイト｜京都社会人サッカー1部",
   description: "京都府社会人サッカー1部リーグに所属するFCアニース京都は、京都府向日市に本拠地を置くアマチュアサッカーチームです。創設30年。向日市を拠点に、京都市・乙訓地域の情熱を背負い、FCアニースは再び加速する。",
+  verification: {
+    google: "hl6nc__17T2aoWGOZ9gjFNe7mif8oC129zuHEiyS22c",
+  },
 };
 
 export default function RootLayout({
